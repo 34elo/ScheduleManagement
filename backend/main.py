19d1c41c859efbd0a6a2a80fea85e20c986d3ebd
elo <evsenik23@mail.ru>
2025-03-04 21:46:19 +0300

    update info.md

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,11 @@
-from fastapi import FastAPI, Security
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
+from app.functions.admin_functions import create_general_report
 from app.routers.admin import admin_router
 from app.routers.auth import auth_router
 from app.routers.employee import employee_router
 from app.routers.general import general_router
-from app.routers.utils import get_current_user
-
-from app.functions.admin_functions import create_general_report
 
 create_general_report('2025-01-01', '2025-04-01')
 
@@ -21,8 +18,6 @@ app.include_router(employee_router)
 app.include_router(general_router)
 app.include_router(admin_router)
 
-
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
@@ -31,7 +26,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 def read_root():
     return {"Ну это": "фиаско"}
-
